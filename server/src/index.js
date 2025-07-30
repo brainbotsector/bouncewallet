ea6920d8f319019479476a9320519dc7e1831218
brainbotsector <88769793+brainbotsector@users.noreply.github.com>
2025-07-31 00:40:06 +0530

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -8,7 +8,14 @@ import { randomUUID } from "crypto";
 dotenv.config({ path: "./.env" });
 console.log("✅ env loaded:", !!process.env.PLAID_CLIENT_ID, !!process.env.PLAID_SECRET);
 
-await mongoose.connect(process.env.MONGO_URI);
+// await mongoose.connect(process.env.MONGO_URI);
+await mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  ssl: true,
+  sslValidate: false,
+  sslCA: "/etc/ssl/certs/ca-certificates.crt",
+});
 
 // ---------- Plaid client ----------
 const plaidClient = new PlaidApi(
